a80980a950d23d77a02401f642013186048a7072
Kennedy <a_kennedy61891@tss-hr420-dm27.fc.ca>
2019-03-04 13:18:55 -0500

    uiUpdates

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
 		// gets generated with every event and passes ome data through
 		//debugger;
 		//
-		//try selecting the proper audio
+		//try selecting the matching audio
 		let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
 		// the return keyword stops javascript from running (any code below it)
@@ -16,7 +16,29 @@
 		//play the audio track
 		audio.currentTime = 0;
 		audio.play();
+
+		//select the parent div and animate it 
+		//..try selecting the matching parent div element
+		let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+		key.classList.add('playing');
+	}
+
+	function removePlayingClass(e) {
+		//listen for a CSS transition to finish and then reset the element
+		//by removing the playing class
+
+		if (e.propertyName !== "transform") {
+			return;
+		} else {
+			console.log('do some stuff here, transition is done');
+			e.currentTarget.classList.remove('playing');
+		}
+		
 	}
 
 	window.addEventListener('keydown', playDrumSound);
+
+	const keys = Array.from(document.querySelectorAll('.key'));
+
+	keys.forEach(key => key.addEventListener('transitionend', removePlayingClass));
 })();
